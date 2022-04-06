bfb0284f439135fa231af3771c258d8afd3b1c5f
Utkarsh Goel <utkarshgoel397@gmail.com>
2022-04-06 15:52:00 +0530

    Set up for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -81,6 +82,6 @@ app.get('*',(req,res)=>{
         name:'Utkarsh'
     })
 })
-app.listen(3000,()=>{
-    console.log('Server is running')
+app.listen(port,()=>{
+    console.log('Server is running at ' + port)
 })
